96601aaa14df29eb82bebb34a3f3b2c991da62d8
Krupa Galiya <krupagaliya@vgecg.ac.in>
2018-01-20 00:50:11 +0530

    .py version added

---
+++ python.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+git init
+
+
+# In[7]:
+
+
+print ("hello world s")
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+a = np.array([(1,2,3),(6,7,8)],dtype=float)
+
+
+# In[5]:
+
+
+a
+
+
+# In[8]:
+
+
+a.max()
+
+
+# In[9]:
+
+
+a
+
